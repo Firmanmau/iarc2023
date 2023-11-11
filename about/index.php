f12c8d40ef4f3a335eef97d447b956f3e1cbbfc9
Firmanmau <firmanfgrt@gmail.com>
2023-11-11 10:24:13 +0700

    IARC 2023

---
+++ about/index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>About IARC 2023</title>
+  <link rel="stylesheet" type="text/css" href="iarc.css">
+  <link rel="stylesheet" href="responsive.css">
+  <link href="https://api.fontshare.com/v2/css?f[]=chillax@1,600&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"> -->
+  <!-- <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
+   <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+   <link rel="stylesheet" type="text/css" href="css/slick.css">
+   <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+</head>
+<body>
+<div class="main-container">
+        <div class="navbar baris">
+                <div class="logo">
+                    <a class="navbar-brand" href="index.php">
+                      <img src="logo/iarc-putih.png" title ="Logo IARC 23">
+                    </a>
+                </div>
+                <div class="menu baris ft-md">
+                  <div><a href="../home.html">Home</a></div>
+                  <div><a href="index.php">About</a></div>
+                  <div><a href="../competition/divisi.html">Competition</a></div>
+                </div>
+                <div class="merchandise baris">
+                  <div class="merchandise-button"><a href="https://instagram.com/iarc.store?igshid=YmMyMTA2M2Y=" >Merchandise</a></div>
+                </div>
+                <div class="hamburger" onclick="doc=document.getElementsByClassName('navbar')[0].getElementsByClassName('collapsible-bar')[0]; if(doc.classList.contains('active')){doc.classList.remove('active')} else {doc.classList.add('active')}">
+                  <svg style="height: 60%; stroke: white; stroke-width: 13;" viewbox="0 0 100 100">
+                    <path d="M10 25 h80 M10 50 h80 M10 75 h80" />
+                  </svg>
+               </div>
+                <div class="collapsible-bar ft-md">
+                	<a href="../home.html"><div class="items" style="text-decoration: none;">Home</div></a>
+                	<a href="index.php"><div class="items" style="text-decoration: none;">About</div></a>
+                	<a href="../competition/divisi.html" style="text-decoration: none;"><div class="items">Competition</div></a>
+                </div>
+         </div>
+
+      <div class="header-start">
+
+          <header>
+            <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500" class="header-content" style="">
+              <h2 style="color: white;" >About</h2>
+              <h2 style="color: white;">IARC 2023</h2>
+            </div>
+         
+          </header>
+      </div>
+       <section class="section-1">
+                <div data-aos="fade-up">
+                    <img src="gambar/garis-22.png" class="gbrgaris22" >
+                </div>
+              <div class="container-2">
+                  
+                       <!-- <div class="container-3">  -->
+                          <div class="box-1">
+                              <div style="height: 50vh;">
+                                <div class="iarc-1">
+                                  <h1  style="text-align: right; color: white;">IARC 1</h1>
+                                </div>
+                                <div class="desc-iarc-1" style="margin-top: 5%;">
+                                  <p style="color: white; margin-top: 5%">
+                                   IARC tahun 2018 merupakan salah satu kegiatan perlombaan yang paling seru dan banyak diminati pada ITS kala itu. Divisi perlombaan yang dilombakan pada tahun itu ada smart soccer car, sumo, line tracer microcontroller, madding electrics, line tracer analog, programmable logic controller, karya tulis ilmiah, dan juga lego nxt. Kegiatan IARC tahun 2018 diadakan di Gedung robotika dan berlangsung selama tiga hari. Banyak bibit-bibit unggul lahir pada kompetisi tahun ini dan juga menjadi bagian dari Departemen Teknik Elektro Otomasi ITS.</p>
+                                </div>
+                              </div>
+                              <div class="sliders-1">
+                                  <div  class="gambar-iarc-1">
+                                        <img style= "width: 100%; margin-top: 3vh;" class="" src="2019/src_sma.jpg">
+                                  </div>
+                                  <div class="gambar-iarc-1" >
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/kti2.jpg">   
+                                  </div>
+                                  <div class="gambar-iarc-1" >
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/plc2.jpg">   
+                                  </div>
+                                  <div class="gambar-iarc-1">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/lego2.jpg">   
+                                  </div>
+                                  <div class="gambar-iarc-1">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/ltm2.jpg">   
+                                  </div>
+                                  <div  class="gambar-iarc-1">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/lta2.jpg">   
+                                  </div>
+                              </div>
+                          </div>
+                          <div data-aos="fade-up" class="box-2">
+                              <div class="tahun-1" data-aos="fade-left"> 
+                                  <h2>
+                                    20
+                                    18
+                                  </h2>
+                                  <h2 style="width: min-content;float: right; margin: 0 10%;font-size: 10vw; line-height: 40%;">
+                                    .
+                                    .
+                                    .
+                                  </h2>
+                              </div>
+                              <div class="tahun-2"  data-aos="fade-right">
+                                  <h2>
+                                    20
+                                    19
+                                  </h2>
+                                  <h2 style="width: min-content;float: left; margin: 0 10%;font-size: 10vw; line-height: 40%;">
+                                    .
+                                    .
+                                    .
+                                  </h2>
+                              </div>
+                          </div> 
+                          <div class="box-3">
+                            <div class="sliders-2">
+                                  <div class="gambar-iarc-2" style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/src3.jpg">   
+                                  </div>
+                                  <div   class="gambar-iarc-2" style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/kti3.jpg">   
+                                  </div>
+                                  <div class="gambar-iarc-2" style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/plc3.jpg">   
+                                  </div>
+                                  <div class="gambar-iarc-2" style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/lego3.jpg">   
+                                  </div>
+                                  <div style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/ltm1.jpg">   
+                                  </div>
+                                  <div  style="height: 50vh;">
+                                      <img style="width: 100%; margin-top: 3vh;" src="2019/lta1.jpg">   
+                                  </div>
+                            </div>
+                              <div class="desc-iarc-2">
+                                    <h1  style="color: white;">IARC 2</h1>
+                                    <p  style="color: white; margin-top: 5%">
+                                    IARC tahun 2019, merupakan perlombaan IARC yang terakhir kalinya diadakan sebelum adanya pandemic covid. Perlombaan IARC tahun 2019 merupakan perlombaan yang paling meriah dengan hampir 600 tim bertanding demi mendapatkan juara. Divisi perlombaan yang dilombakan pada tahun itu sama dengan perlombaan-perlombaan pada IARC sebelumnnya. Kegiatan IARC tahun 2019 juga diadakan di Gedung Robotika dan berlangsung selama tiga hari. IARC tahun 2019 menyimpan banyak bekas dan pengalaman bagi para peserta dan juga panitia. </p>
+
+                              </div>
+                          </div>
+              </div>     
+                  <!-- </div> -->
+                     
+            <div class="pembungkus">
+                        <div class="container-4">
+                          <img src="gambar/atribut-1.png" style="position: absolute; width: 100%;" >
+                                <div class="hidden" style="visibility: hidden;">
+                                    <br><br>               
+                                </div>
+                                <div class="goals"> 
+                                    <h1 style="text-align:center; color: #4cc9e0;" >GOALS</h1>
+                                </div>
+                                  
+                        </div>
+                
+
+                      <div class="container-5">
+                            <div >
+                                <img class="asset-5" src="gambar/asset-5.svg">
+                            </div>
+                            <div  class="text-goals">
+                                <p>Berkontribusi memajukan indonesia dalam bidang Teknik otomasi dan robotika dengan IARC sebagai wadah untuk para siswa dan mahasiswa Indonesia agar dapat mengembangkan kompetensi dan juga inovasi dalam bidang Teknik otomasi dan robotika. </p>
+                            </div>
+                            <div >
+                               <img class="asset-5-2" src="gambar/asset-5.svg" >
+                            </div>
+                          
+                      </div> 
+              
+            </div>  
+        </section>
+
+</div>
+
+
+  <div class="footer baris">
+      <div class="footer-left baris">
+        <div class="footer-logo"><img src="logo/iarc-warna.png" alt="Logo IARC 2023" ></div>
+        <div class="accounts">
+          <div class="ft-bg">Official Acount</div>
+          <div class="ft-sm">IARC Official</div>
+          <div class="ft-sm">@IARC_Official</div>
+          <div class="ft-sm">@IARC_Official</div>
+        </div>
+      </div>
+      <div class="footer-right baris">
+        <div class="sponsors">
+          <h3 class="ft-bg">Sponsors</h3>
+          <div class="sponsor-logos-container baris">
+            <img src="gambar/robokidz.jpeg">
+          </div>
+          <div class="sponsor-logos-container2">
+						<img src="gambar/laderm.png">
+					</div>
+        </div>
+        <div class="legals baris">
+          <div class="logo">
+              <img src="gambar/asset25.png" >
+          </div>
+        </div>
+      </div>
+    </div>  
+  <script type="text/javascript">
+    const docs = [document.getElementById("divisi-scroll"),document.getElementById("info-switch"),document.getElementById("list-switch")]
+    var active = 0;
+    function scrolls(direction){
+      if (active + direction >= 0 && active + direction < 8){
+        active += direction
+        docs.forEach(function(e){
+          cmd = e.id.split("-")
+          if (cmd[1] == "scroll"){
+            e.scroll(0,e.offsetHeight*active)
+          } else if (cmd[1] == "switch"){
+            e.children[active-direction].classList.remove("active")
+            e.children[active].classList.add("active")
+          }
+        })
+        document.getElementById("divisi-input").value = active+1
+      }
+    }
+  </script>
+  <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script> -->
+  <script type="text/javascript">
+   $(document).ready(function(){
+      $('.sliders-1, .sliders-2').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      cssEase: 'linear'
+
+      });
+    });
+  </script>
+  <!-- <script type="js/app.js"></script> -->
+  <script type="text/javascript" src="js/slick.min.js"></script>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script> AOS.init();</script>
+</body>
+</html>
